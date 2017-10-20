3bde5ca83a71345610c571a68d55ef3ed3db48a5
Luke Walsh <lwalsh@kgsbuildings.com>
2017-10-20 15:33:02 -0400

    added sample table to testview

--- demo/src/TestView.js
+++ demo/src/TestView.js
@@ -1,14 +1,54 @@
 import React from 'react';
-import FlatButton from 'material-ui/FlatButton';
+import {
+    Table,
+    TableBody,
+    TableHeader,
+    TableHeaderColumn,
+    TableRow,
+    TableRowColumn,
+  } from 'material-ui/Table';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class TestView extends React.Component {
   render() {
     return (
         <MuiThemeProvider>
-          <FlatButton label='TestButton'>
-              OK
-          </FlatButton>
+          <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHeaderColumn>ID</TableHeaderColumn>
+              <TableHeaderColumn>Name</TableHeaderColumn>
+              <TableHeaderColumn>Description</TableHeaderColumn>
+              <TableHeaderColumn>Value</TableHeaderColumn>
+            </TableRow>
+          </TableHeader>     
+            <TableBody>
+            <TableRow>
+              <TableRowColumn>1</TableRowColumn>
+              <TableRowColumn>TestName 1</TableRowColumn>
+              <TableRowColumn>Description 1!</TableRowColumn>
+              <TableRowColumn>23.4</TableRowColumn>
+            </TableRow>
+            <TableRow>
+              <TableRowColumn>2</TableRowColumn>
+              <TableRowColumn>Namers</TableRowColumn>
+              <TableRowColumn>It's a trap!</TableRowColumn>
+              <TableRowColumn>13.4</TableRowColumn>
+            </TableRow>
+            <TableRow>
+              <TableRowColumn>3</TableRowColumn>
+              <TableRowColumn>John</TableRowColumn>
+              <TableRowColumn>blah de blah</TableRowColumn>
+              <TableRowColumn>40</TableRowColumn>
+            </TableRow>
+            <TableRow>
+              <TableRowColumn>4</TableRowColumn>
+              <TableRowColumn>Name4</TableRowColumn>
+              <TableRowColumn>hello world</TableRowColumn>
+              <TableRowColumn>100</TableRowColumn>
+            </TableRow>                        
+            </TableBody> 
+          </Table>
         </MuiThemeProvider>
     );
   };
